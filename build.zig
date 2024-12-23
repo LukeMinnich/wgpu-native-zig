44ea9f4851efc4b190875bc9857341067fbead99
LukeMinnich <squareman2357@gmail.com>
2024-12-23 12:49:53 -0800

    Update `wgpu` dependencies to v22.1.0.5

--- build.zig
+++ build.zig
@@ -71,11 +71,11 @@ pub fn build(b: *std.Build) void {
         // There's also some sorf of .pdb file available, which I think is a database of debugging symbols.
         // I don't even have a Windows machine though,
         // so I'll let somebody who does tell me what it is and if I need it.
-        .windows => "wgpu_native.lib",
+        .windows => "lib/wgpu_native.lib",
 
         // This only tries to account for linux/macos since we're using pre-compiled wgpu-native;
         // need to think harder about this if I get custom builds working.
-        else => "libwgpu_native.a",
+        else => "lib/libwgpu_native.a",
     }; // Also these don't even try to account for dynamic linking; that's a problem for future me.
     const libwgpu_path = wgpu_dep.path(lib_name);
     mod.addObjectFile(libwgpu_path);
